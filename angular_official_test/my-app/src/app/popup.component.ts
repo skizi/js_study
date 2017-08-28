c0e737fc05c0373bc6c7d2781b15f20a45fd2b66
skizi <skizi3024@gmail.com>
2017-08-29 00:53:21 +0900

    モーダル実装 コンポーネントに値を渡す コンポーネントの再描画時にイベントを実行するように リサイズイベント

--- angular_official_test/my-app/src/app/popup.component.ts
+++ angular_official_test/my-app/src/app/popup.component.ts
@@ -1,20 +1,53 @@
-import { Component } from '@angular/core';
+/// <reference path="../typings/globals/jquery/index.d.ts" />
+
+import { Component, Input, AfterViewInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'popup-component',
   templateUrl: './popup.component.html',
   styleUrls: ['./popup.component.scss']
 })
-export class PopupComponent {
-  
-  text:string = '1111';
+export class PopupComponent implements AfterViewInit, AfterViewChecked {
 
-  setText( text ) {
-  	console.log(text);
-  	this.text = text;
+  private _newsText:string;
+
+
+  ngInit(){
+  	console.log("init");
   }
 
-  popupClick(){
+
+  @Input()
+  set newsText(newsText: string) {
+  	this._newsText = newsText;
+  	$('#popup').show();
+  }
+
+
+  get newsText(): string {
+    return this._newsText.toUpperCase();
+  }
+
+
+  ngAfterViewInit(){
+  	
+  	$( '#popup' ).hide();
 
   }
+
+
+  ngAfterViewChecked(){
+
+  	var popup = $('#popup');
+  	var x = -popup.width() * .5;
+  	var y = -popup.height() * .5;
+  	popup.css({ marginLeft : x + 'px', marginTop : y + 'px' });
+
+  }
+
+
+  closeBtnClick(){
+  	$('#popup').hide();
+  }
+
 }
