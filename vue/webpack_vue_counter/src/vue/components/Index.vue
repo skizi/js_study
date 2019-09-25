ac255436e14c629f77524aa4ccd13167e099da5e
skizi <>
2019-09-25 23:35:37 +0900

    fetch、axiosのテスト

--- vue/webpack_vue_counter/src/vue/components/Index.vue
+++ vue/webpack_vue_counter/src/vue/components/Index.vue
@@ -12,6 +12,12 @@
 
       </div>
      </main>
+
+
+    <form @submit.prevent="postAxiosForm" >
+        <input type="text" name="hoge" >
+        <button type="submit" >test</button>
+    </form>
     
      <img src="/assets/img/db3ec985ddafea6bed1cb04b95337a95_600.jpg">
 
@@ -35,6 +41,7 @@
 
 <script>
 import Modal from './Modal'
+import axios from 'axios';
 
 export default {
 
@@ -70,7 +77,100 @@ export default {
         this.$store.commit( 'showModalFlag', true );
       }
 
-    }
+    },
+
+    postJson( e ){
+
+      var data = {
+        hoge : e.target.hoge.value
+      };
+      const headers = {
+        'Content-Type': 'application/json'
+      };
+
+      fetch('https://example.com/profile/avatar', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers : headers
+      })
+        .then(response => console.log('Success:', JSON.stringify(response)))
+        .catch(error => console.error('Error:', error));
+
+    },
+
+    postForm( e ){
+
+      var formData = new FormData();
+      formData.append('hoge', e.target.hoge.value);
+
+      fetch('https://example.com/profile/avatar', {
+        method: 'POST',
+        body: formData
+      })
+        .then(response => console.log('Success:', JSON.stringify(response)))
+        .catch(error => console.error('Error:', error));
+
+    },
+
+    getJson( e ){
+
+      let params = new URLSearchParams();
+      params.set('hoge', e.target.hoge.value);
+
+      fetch('https://example.com/profile/avatar?' + params.toString())
+        .then(response => console.log('Success:', JSON.stringify(response)))
+        .catch(error => console.error('Error:', error));
+
+    },
+
+
+    postAxiosJson( e ){
+      
+      var data = {
+        hoge : e.target.hoge.value
+      };
+
+      axios.post('https://example.com/profile/avatar', data )
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+    },
+
+
+    postAxiosForm( e ){
+      
+      var formData = new FormData();
+      formData.append('hoge', e.target.hoge.value);
+
+      axios.post('https://example.com/profile/avatar', formData )
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+    },
+
+
+    getAxios( e ){
+      
+      let params = new URLSearchParams();
+      params.set('hoge', e.target.hoge.value);
+
+      axios.get('https://example.com/profile/avatar?' + params.toString())
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+    },
 
   },
 
