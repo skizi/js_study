98f36a71e3c5b8ab645a658c770bc2062c1ced3d
yoshida-mamoru <>
2020-04-16 21:49:16 +0900

    nuxtでaxios、storeのテスト

---
+++ nuxt/test1/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+
+    <header>
+      <h1>Header1</h1>
+      <button @click="clickBtn">btn</button>
+      <input type="text" v-model="code">
+
+	  <button @click="submit">submit</button>      
+    </header>
+
+</template>
+
+
+<script>
+import axios from "axios";
+
+export default {
+
+	name : "Header",
+
+
+	data(){
+
+		return {
+			code:0,
+			loadingFlag:false
+		}
+
+	},
+
+
+	mounted(){
+
+		console.log("header");
+
+	},
+
+
+	methods : {
+
+		clickBtn(){
+
+			console.log( "clickBtn" );
+
+		},
+
+
+		submit(){
+
+			if( this.loadingFlag ) return;
+			this.loadingFlag = true;
+
+
+      		this.$store.dispatch( 'getData', { zipcode:this.code } ).then( response => {
+				if( response.status == "success" ){
+					console.log( response );
+				}else{
+					console.log( response );
+					console.log( "error!!" );
+				}
+				this.loadingFlag = false;
+			} );
+
+		},
+
+	}
+	
+}
+
+</script>
---
+++ nuxt/test1/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <header-component></header-component>
+    <section>
+      <h2>Title1</h2>
+      <p>texttexttexttexttexttexttext</p>
+    </section>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '~/components/Header.vue'
+
+export default {
+  components: {
+    HeaderComponent
+  }
+}
+</script>
+
+<style>
+  
+</style>
