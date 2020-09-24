f02e3df463faaab416ec46fa40922a9016b80138
yoshida-mamoru <skizi3024@gmail.com>
2020-09-25 00:18:14 +0900

    useRefを試した

---
+++ next/test0/components/organisms/ref-test.tsx
@@ -0,0 +1,31 @@
+import React, {useRef} from 'react';
+
+
+const RefTest: React.FC = () => {
+
+  const ref = useRef();
+
+  const onClick = () => {
+    ref.current.focus();
+  };
+
+
+  return (
+    <>
+      <style jsx>
+        {`
+          h3{
+            padding-left:4px;
+            border-left:2px solid #333;
+          }
+        `}
+      </style>
+      <h3>Ref Test</h3>
+    	<button onClick={onClick}>Set Focus</button>
+		  <input type="text" ref={ref}></input>
+    </>
+  );
+
+};
+
+export default RefTest;
--- next/test0/pages/index.tsx
+++ next/test0/pages/index.tsx
@@ -2,6 +2,8 @@ import LoadingToggle from '~/components/organisms/loading-toggle';
 import LoginContainer from '~/components/organisms/login-container';
 import ZipContainer from '~/components/organisms/zip-container';
 
+import RefTest from '~/components/organisms/ref-test'
+
 
 const Index: React.FC = () => {
 
@@ -10,6 +12,7 @@ const Index: React.FC = () => {
 		<LoadingToggle title="ローディングをトグルする"></LoadingToggle>
 		<LoginContainer title="ログイン"></LoginContainer>
 		<ZipContainer></ZipContainer>
+		<RefTest></RefTest>
 	</>
   );
 
