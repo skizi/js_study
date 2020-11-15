df719b06493d12329be07a070bcd5feb6d7f27d2
yoshida-mamoru <skizi3024@gmail.com>
2020-11-15 20:41:24 +0900

    Addressのテストを記述

--- react/form0/src/components/molecules/Address.tsx
+++ react/form0/src/components/molecules/Address.tsx
@@ -30,18 +30,34 @@ const Address:React.FC = () => {
 				<TextField fullWidth className={classes.formField} label={PROFILE.ADDRESS.POSTALCODE} onChange={e=>handlePostalcodeChange( e.target.value )}
 		        required
 		        error={!!validation.message.address.postalcode}
-		        helperText={validation.message.address.postalcode} />
+		        helperText={validation.message.address.postalcode}
+			    inputProps={{
+			        "data-testid": "postalcode",
+			    }}
+		        />
 				<TextField fullWidth className={classes.formField} label={PROFILE.ADDRESS.PREFECTURE} value={prefecture} onChange={e=>handleAddressChange( { prefecture:e.target.value } )}
 		        required
 		        error={!!validation.message.address.prefecture}
-		        helperText={validation.message.address.prefecture} />
+		        helperText={validation.message.address.prefecture}
+			    inputProps={{
+			        "data-testid": "prefecture",
+			    }}
+		        />
 				<TextField fullWidth className={classes.formField} label={PROFILE.ADDRESS.CITY} value={city} onChange={e=>handleAddressChange( { city:e.target.value } )}
 		        required
 		        error={!!validation.message.address.city}
-		        helperText={validation.message.address.city} />
+		        helperText={validation.message.address.city}
+			    inputProps={{
+			        "data-testid": "city",
+			    }}
+		        />
 				<TextField fullWidth className={classes.formField} label={PROFILE.ADDRESS.RESTADDRESS} onBlur={e=>handleAddressChange( { restAddress:e.target.value } )}
 		        error={!!validation.message.address.restAddress}
-		        helperText={validation.message.address.restAddress} />
+		        helperText={validation.message.address.restAddress}
+			    inputProps={{
+			        "data-testid": "restAddress",
+			    }}
+		        />
 			</>
 		);
 	}, [validation.message.address, city, prefecture]);
